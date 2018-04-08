db579617337f8541146eabfd7fa08be611279b49
dipti <Gargdipti12@yahoo.com>
2018-04-08 15:19:27 +0530

    Refactored Ui Code for Demo

---
+++ DynamoDb/UIHtmlSimple/js/monitoring.js
@@ -0,0 +1,60 @@
+var app = angular.module('monitoringApp',[]);
+
+ app.controller('monitoringController', function($scope) {
+        //$scope.tableData = {}; 
+        var headers = ["S. No.", "Discharge Pressure", "Discharge Temperature","Suction Pressure", "Suction Temperature", "Fluid Flow", "Pump Effeciency", "Dynamic Head", "Time Stamp"];
+        var tableData = [];
+    
+        var awsConfig = {
+            
+            "region" : region,
+            // "endpoint" :endpoint,
+            "accessKeyId" : accessKeyId,
+            "secretAccessKey" : secretAccessKey	
+        };
+        AWS.config.update(awsConfig);
+        var docClient = new AWS.DynamoDB.DocumentClient();
+        var params = {
+            TableName : "MeasuredData"
+        };	
+
+        docClient.scan(params,function(err, data){
+        if(err)
+        {
+        console.log("error");
+        }
+        else 
+        {
+        console.log("success");
+        tableData = [];
+        for(var index=0; index<data.Items.length ;index++){
+            var rowData = [];
+            rowData.push(index+1);
+            rowData.push(data.Items[index].DischargePressure);
+            rowData.push(data.Items[index].DischargeTemp);
+            rowData.push(data.Items[index].SuctionPressure);
+            rowData.push(data.Items[index].SuctionTemp);
+            rowData.push(data.Items[index].FluidFlow);
+
+            var pumpEff = String(data.Items[index].PumpEffeciency);
+            pumpEff =  pumpEff.substring(0,5);
+            rowData.push(pumpEff);
+            
+            var dyHEad = String(data.Items[index].DynamicHead);
+            dyHEad =  dyHEad.substring(0,5);
+            rowData.push(dyHEad);
+
+            rowData.push(data.Items[index].TimeStamp); 
+
+            tableData.push(rowData);
+        
+        }		
+
+        $scope.headers=headers;
+        $scope.tableData=tableData;	
+        $scope.$apply();
+        }		
+        
+	});
+	
+});
