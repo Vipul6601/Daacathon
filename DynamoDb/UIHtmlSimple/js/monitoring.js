e24a6a92992f588de6287330e526ca46ad782711
dipti <Gargdipti12@yahoo.com>
2018-04-13 15:10:39 +0530

    Code by dipti

--- DynamoDb/UIHtmlSimple/js/monitoring.js
+++ DynamoDb/UIHtmlSimple/js/monitoring.js
@@ -22,79 +22,83 @@ app.controller('monitoringController', ['$scope', '$interval', function ($scope,
         ScanIndexForward: true
     };
 
-    docClient.scan(params, function (err, data) {
-        if (err) {
-            console.log("error");
-        }
-        else {
-            console.log("success");
-            tableData = [];
-            for (var index = 0; index < data.Items.length; index++) {
-                var rowData = [];
-                rowData.push(index + 1);
-                rowData.push(data.Items[index].DischargePressure);
-                rowData.push(data.Items[index].DischargeTemp);
-                rowData.push(data.Items[index].SuctionPressure);
-                rowData.push(data.Items[index].SuctionTemp);
-                rowData.push(data.Items[index].FluidFlow);
-
-                var pumpEff = String(data.Items[index].PumpEffeciency);
-                pumpEff = pumpEff.substring(0, 5);
-                rowData.push(pumpEff);
-
-                var dyHEad = String(data.Items[index].DynamicHead);
-                dyHEad = dyHEad.substring(0, 5);
-                rowData.push(dyHEad);
-
-                rowData.push(data.Items[index].TimeStamp);
-
-                tableData.push(rowData);
-
-                if (index == data.Items.length - 1) {
-                    $scope.motorStatus = data.Items[index].MotorStatus;
-                }
-
+    $interval(function() {
+        docClient.scan(params, function (err, data) {
+            if (err) {
+                console.log("error");
             }
+            else {
+                console.log("success");
+                tableData = [];
+                for (var index = 0; index < data.Items.length; index++) {
+                    var rowData = [];
+                    rowData.push(index + 1);
+                    rowData.push(data.Items[index].DischargePressure);
+                    rowData.push(data.Items[index].DischargeTemp);
+                    rowData.push(data.Items[index].SuctionPressure);
+                    rowData.push(data.Items[index].SuctionTemp);
+                    rowData.push(data.Items[index].FluidFlow);
+    
+                    var pumpEff = String(data.Items[index].PumpEffeciency);
+                    pumpEff = pumpEff.substring(0, 5);
+                    rowData.push(pumpEff);
+    
+                    var dyHEad = String(data.Items[index].DynamicHead);
+                    dyHEad = dyHEad.substring(0, 5);
+                    rowData.push(dyHEad);
+    
+                    rowData.push(data.Items[index].TimeStamp);
+    
+                    tableData.push(rowData);
+    
+                    if (index == data.Items.length - 1) {
+                        $scope.motorStatus = data.Items[index].MotorStatus;
+                    }
+    
+                }
+    
+                $scope.headers = headers;
+                $scope.tableData = tableData;
+                $scope.$apply();
+            }
+    
+            $interval(function(){
+                var params = {
+                    TableName: "AlarmTable",
+                    FilterExpression: "#status = :status",
+                    ExpressionAttributeNames:{"#status": "Status"},
+                    ExpressionAttributeValues: {
+                        ":status": "Active",
+                    }
+               };
+            
+                docClient.scan(params, function (err, data) {
+                    if (err) {
+                        console.log("error");
+                    }
+                    else {
+                        console.log("success");
+                       var alarms = [];
+                        for (var index = 0; index < data.Items.length; index++) {
+                            var alarmData= {};
+                            
+                            alarmData.Name = data.Items[index].Name;
+                            alarmData.TimeStamp = data.Items[index].TimeStamp;
+                            alarmData.Status = data.Items[index].Status;
+                            alarmData.Type  = data.Items[index].Type;
+                            alarms.push(alarmData);
+            
+                       }
+            
+                        $scope.alarms = alarms;
+                        $scope.$apply();
+                    }
+                });
+            }, 1000);
+        });
+    }, 1000); 
 
-            $scope.headers = headers;
-            $scope.tableData = tableData;
-            $scope.$apply();
-        }
-
-        $interval(function(){
-        	var params = {
-                TableName: "AlarmTable",
-                FilterExpression: "#status = :status",
-                ExpressionAttributeNames:{"#status": "Status"},
-                ExpressionAttributeValues: {
-                    ":status": "Active",
-                }
-           };
-        
-            docClient.scan(params, function (err, data) {
-                if (err) {
-                    console.log("error");
-                }
-                else {
-                    console.log("success");
-                   var alarms = [];
-                    for (var index = 0; index < data.Items.length; index++) {
-                        var alarmData= {};
-                        
-                        alarmData.Name = data.Items[index].Name;
-                        alarmData.TimeStamp = data.Items[index].TimeStamp;
-                        alarmData.Status = data.Items[index].Status;
-                        alarmData.Type  = data.Items[index].Type;
-                        alarms.push(alarmData);
-        
-                   }
-        
-                    $scope.alarms = alarms;
-                    $scope.$apply();
-                }
-            });
-        }, 1000);
-    });
+   
 
 
 }]);
